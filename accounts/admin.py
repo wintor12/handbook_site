f72198a325643db2aa729f10e0423e4e2342859b
Tong Wang <tonwng@amazon.com>
2022-02-20 13:36:39 -0500

    update account model category relationship

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,10 @@
 Admin
 """
 
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Project, Category, Pro
+
+admin.site.register(Project)
+admin.site.register(Category)
+admin.site.register(Pro)
--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,49 @@
 Models
 """
 
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Category
+    """
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Pro(models.Model):
+    """
+    Pro
+    """
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True, blank=True)
+    email = models.CharField(max_length=200, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Project(models.Model):
+    """
+    Project
+    """
+    STATUS = (
+        ('Open', 'Open'),
+        ('Assigned', 'Assigned'),
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+    )
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000, blank=True)
+    pro = models.ForeignKey(Pro, null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ManyToManyField(Category, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    status = models.CharField(max_length=200, null=True, blank=True, choices=STATUS)
+
+    def __str__(self):
+        return str(self.name)
